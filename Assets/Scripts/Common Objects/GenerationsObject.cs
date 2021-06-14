624707113e83b3fbb76a791eb5e0624745f3da73
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:27:17 -0300

    Created Generations Object
    Class base for a generations objects

---
+++ Assets/Scripts/Common Objects/GenerationsObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class GenerationsObject : RingEngineObject
+{
+    public bool active
+    {
+        get
+        {
+            return Active;
+        }
+        set
+        {
+            if (Active != value)
+            {
+                if (Active == false)
+                {
+                    SendMessage("OnBecomeActive", SendMessageOptions.DontRequireReceiver);
+                }
+                else
+                {
+                    SendMessage("OnBecomeInactive", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+
+            Active = value;
+        }
+    }
+
+    private bool Active;
+
+    public int SetObjectID;
+
+    public Player player { get; set; }
+
+    public GenerationsObject FindObjectByID(int id)
+    {
+        GenerationsObject[] gos = FindObjectsOfType<GenerationsObject>();
+
+        foreach (GenerationsObject go in gos)
+        {
+            if (go.SetObjectID == id)
+            {
+                return go;
+            }
+        }
+        return null;
+    }
+
+    public virtual void OnValidate()
+    {
+
+    }
+}
+
