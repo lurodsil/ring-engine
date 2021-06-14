b3b18eb9e00c9c3aa38c6328cb3204f8d7e52d61
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:16:50 -0300

    Created Blink Materials
    can be used on boss damage

---
+++ Assets/Scripts/BlinkMaterials.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BlinkMaterials : MonoBehaviour
+{
+    public Material[] materials;
+    public int numberOfTimes = 10;
+    public float interval = 0.05f;
+    public Color color = Color.white;
+
+    public void Blink()
+    {
+        StartCoroutine(BlinkWhite(numberOfTimes, interval));
+    }
+    private IEnumerator BlinkWhite(int times, float interval)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            foreach (Material m in materials)
+            {
+                m.SetColor("_EmissionColor", color);
+            }
+            yield return new WaitForSeconds(interval);
+            foreach (Material m in materials)
+            {
+                m.SetColor("_EmissionColor", Color.black);
+            }
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
