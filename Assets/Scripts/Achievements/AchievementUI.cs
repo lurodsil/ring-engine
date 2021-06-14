aa7f04c78387a2f0a124afc0b3f77db67750a11f
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:14:13 -0300

    Created Achievements

---
+++ Assets/Scripts/Achievements/AchievementUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementUI : MonoBehaviour
+{
+    public float autoHideTime = 5;
+    public Image achievement;
+    public Text achievementUnlocked;
+    public Text achievementReason;
+    public Text achievementName;
+    private Vector2 show = new Vector2(0, 64);
+    private Vector2 hide = new Vector2(0, -64);
+    private Vector2 target;
+
+    void Awake()
+    {
+        target = hide;
+        achievement.rectTransform.anchoredPosition = target;
+    }
+
+    void Update()
+    {
+        achievement.rectTransform.anchoredPosition = Vector2.Lerp(achievement.rectTransform.anchoredPosition, target, 10 * Time.deltaTime);
+    }
+
+    public void Show(Achievement achievement, LanguageType language)
+    {
+        achievementUnlocked.text = language == LanguageType.English ? "achievement unlocked" : "conquista desbloqueada";
+        achievementReason.text = language == LanguageType.English ? achievement.reasonEn : achievement.reasonPt;
+        achievementName.text = language == LanguageType.English ? achievement.achievmentEn : achievement.achievmentPt;
+        target = show;
+        StartCoroutine(AutoHide(autoHideTime));
+    }
+
+    public void Hide()
+    {
+        target = hide;
+    }
+
+    IEnumerator AutoHide(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Hide();
+    }
+}
---
+++ Assets/Scripts/Achievements/Achievements.cs
@@ -0,0 +1,35 @@
+﻿public static class Achievements
+{
+
+    public static Achievement[] achievements = new Achievement[]
+    {
+        new Achievement("Welcome to Ring Engine!", "Bem vindo a Ring Engine!", "Started the game", "Iniciou o jogo"),
+        new Achievement("Ah! One extra life!", "Ah! Uma vida extra!", "Collected 100 Rings", "Coletou 100 Rings"),
+        new Achievement("Die Without Fear!", "Die Without Fear!", "Get the first Checkpoint", "Get the first Checkpoint")
+    };
+
+    public static int startedTheGameId = 0;
+    public static int collected100Rings = 1;
+    public static int getCheckpoint = 2;
+}
+
+public class Achievement
+{
+    public string achievmentEn;
+    public string achievmentPt;
+    public string reasonEn;
+    public string reasonPt;
+
+    public Achievement()
+    {
+
+    }
+
+    public Achievement(string achievmentEn, string achievmentPt, string reasonEn, string reasonPt)
+    {
+        this.achievmentEn = achievmentEn;
+        this.achievmentPt = achievmentPt;
+        this.reasonEn = reasonEn;
+        this.reasonPt = reasonPt;
+    }
+}
