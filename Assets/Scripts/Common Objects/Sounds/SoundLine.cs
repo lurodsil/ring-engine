c0555c9fd930d27a79ac5e91ac339beec6a4128f
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:28:15 -0300

    Created Sonic Generations Sounds Scripts
    Still a lot to fix and in the future generations dependency will be removed

---
+++ Assets/Scripts/Common Objects/Sounds/SoundLine.cs
@@ -0,0 +1,17 @@
+public class SoundLine : GenerationsObject
+{
+
+    public float BaseVolume = 1f;
+    public string CueName = "4512_waterfall01";
+    public string FileName = "42AMB_GHZ";
+
+    public float IntervalTime = 5f;
+
+    public bool IsRegularly = false;
+    public float Radius = 120f;
+
+    public override void OnValidate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Common Objects/Sounds/SoundPoint.cs
@@ -0,0 +1,19 @@
+public class SoundPoint : GenerationsObject
+{
+
+    public float BaseVolume = 1f;
+    public string CueName = "4512_waterfall01";
+    public string FileName = "42AMB_GHZ";
+
+    public float InsideRadius = 5f;
+    public float IntervalTime = 5f;
+
+    public bool IsRegularly = false;
+    public float Radius = 60f;
+
+
+    public override void OnValidate()
+    {
+
+    }
+}
