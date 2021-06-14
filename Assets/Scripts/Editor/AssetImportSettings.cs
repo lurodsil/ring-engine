8e490db6d4fdf3902275912bd4ed71e70a608135
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:37:11 -0300

    Created Asset Post Processor

---
+++ Assets/Scripts/Editor/AssetImportSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetsImportSettings : AssetPostprocessor
+{
+    void OnPreprocessAnimation()
+    {
+        ModelImporter importer = assetImporter as ModelImporter;
+        //Model
+        importer.materialName = ModelImporterMaterialName.BasedOnTextureName;
+        //importer.materialSearch = ModelImporterMaterialSearch.RecursiveUp;
+        //Animations
+        //importer.animationCompression = ModelImporterAnimationCompression.Off;
+
+        ModelImporterClipAnimation[] animations = importer.defaultClipAnimations;
+
+        Debug.Log(importer.clipAnimations.Length);
+
+        for (int i = 0; i < animations.Length; i++)
+        {
+            string name = Path.GetFileNameWithoutExtension(importer.assetPath);
+            animations[i].name = name;
+            if (name.Contains("_loop"))
+            {
+                animations[i].loopTime = true;
+            }
+        }
+
+        importer.clipAnimations = animations;
+    }
+}
+
