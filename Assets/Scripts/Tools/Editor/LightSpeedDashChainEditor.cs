456e0af294d2094104f2693d618b2475e2942eea
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:45:50 -0300

    Created Tools

---
+++ Assets/Scripts/Tools/Editor/LightSpeedDashChainEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LightSpeedDashChain))]
+public class LightSpeedDashChainEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LightSpeedDashChain lightSpeedDashChain = (LightSpeedDashChain)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Create Chain"))
+        {
+            lightSpeedDashChain.CreateChain(lightSpeedDashChain.ring, lightSpeedDashChain.maxDistance, lightSpeedDashChain.precision);
+        }
+
+        if (GUILayout.Button("Clean Chain"))
+        {
+            lightSpeedDashChain.CleanChain();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Tools/LightSpeedDashChain.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BezierSpline))]
+public class LightSpeedDashChain : MonoBehaviour
+{
+    public GameObject ring;
+    public float maxDistance = 1;
+    public float precision = 0.001f;
+    private BezierSpline bezierSpline;
+
+    public void CleanChain()
+    {
+        int childCount = transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+    }
+
+    public void CreateChain(GameObject prefab, float maxDistance, float precision = 0.001f)
+    {
+        CleanChain();
+
+        if (prefab == null)
+        {
+            Debug.LogWarning("Please setup the ring prefab before creating the chain!");
+            return;
+        }
+
+        bezierSpline = GetComponent<BezierSpline>();
+
+        Vector3 lastPoint = new Vector3();
+
+        gameObject.name = "LightSpeedDashChain";
+
+        Instantiate(ring, bezierSpline.GetPoint(0), Quaternion.identity, transform).GetComponent<Ring>().IsLightSpeedDashTarget = true;
+
+        for (float f = 0; f < 1; f += precision)
+        {
+            Vector3 point = bezierSpline.GetPoint(f);
+
+            if (Vector3.Distance(point, lastPoint) > maxDistance)
+            {
+                Instantiate(ring, point, Quaternion.identity, transform).GetComponent<Ring>().IsLightSpeedDashTarget = true;
+
+                lastPoint = point;
+            }
+        }
+
+        Instantiate(ring, bezierSpline.GetPoint(1), Quaternion.identity, transform).GetComponent<Ring>().IsLightSpeedDashTarget = true;
+    }
+}
