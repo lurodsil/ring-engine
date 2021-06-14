a2fc300420b871ee7e99cfc68b5c7e71b91d77b9
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:35:00 -0300

    Created Cutscene trigger

---
+++ Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    public Transform playerPositionAfterAnimationIsFinished;
+
+    public bool lockPlayerControls = true;
+
+    private Player player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            playableDirector.Play();
+            player = other.GetComponent<Player>();
+            player.velocity = 0;
+            player.rigidbody.velocity = Vector3.zero;
+            if (lockPlayerControls)
+            {
+                player.stateMachine.Pause();
+            }
+        }
+    }
+
+    public void AnimationFinished()
+    {
+        if (playerPositionAfterAnimationIsFinished)
+        {
+            player.transform.position = playerPositionAfterAnimationIsFinished.position;
+            player.transform.rotation = playerPositionAfterAnimationIsFinished.rotation;
+        }
+
+        if (lockPlayerControls)
+        {
+            player.stateMachine.Play();
+        }
+    }
+}
