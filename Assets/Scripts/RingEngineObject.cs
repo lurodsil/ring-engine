f92ceb968482e6e44b89fb52288a6349e1b0ce19
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 17:09:36 -0300

    Update RingEngineObject.cs

--- Assets/Scripts/RingEngineObject.cs
+++ Assets/Scripts/RingEngineObject.cs
@@ -5,8 +5,12 @@ public abstract class RingEngineObject : MonoBehaviour
 {
     public Player player { get; set; }
 
+    [HideInInspector]
     public UnityEvent OnBecomeActive;
+    [HideInInspector]
     public UnityEvent OnBecomeInactive;
+
+    public bool showEventsInInspector { get; set; }
 
     public bool active
     {
@@ -50,5 +54,29 @@ public abstract class RingEngineObject : MonoBehaviour
             GameManager.OnPause -= GetComponent<AudioSource>().Pause;
             GameManager.OnResume -= GetComponent<AudioSource>().UnPause;
         }
+    }
+
+    public void DrawEventLines(UnityEvent unityEvent, Color color)
+    {
+        Gizmos.color = color;
+
+        for (int i = 0; i < unityEvent.GetPersistentEventCount(); i++)
+        {
+            Component targetComponent = unityEvent.GetPersistentTarget(i) as Component;            
+
+            if (targetComponent != null)
+            {
+                Gizmos.DrawLine(transform.position, targetComponent.transform.position);
+            }
+        }
+    }
+
+    public virtual void Activate()
+    {
+        active = true;
+    }
+    public virtual void Deactivate()
+    {
+        active = false;
     }
 }
