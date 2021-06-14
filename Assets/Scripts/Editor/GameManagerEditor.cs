f852baec1c470562fb2a81f4ebe85677d2f41011
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:37:47 -0300

    Created Game Manager Editor

---
+++ Assets/Scripts/Editor/GameManagerEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GameManager gameManager = (GameManager)target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Save Settings"))
+        {
+            FileManager.Save(gameManager.settings, gameManager.settingsPath);
+        }
+
+        if (GUILayout.Button("Load Settings"))
+        {
+            gameManager.settings = FileManager.Load(gameManager.settings, gameManager.settingsPath);
+        }
+
+        if (GUILayout.Button("Show"))
+        {
+            gameManager.achievementUI.Show(Achievements.achievements[1], gameManager.settings.languageType);
+        }
+    }
+}
