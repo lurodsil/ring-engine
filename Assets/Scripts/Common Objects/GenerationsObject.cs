37a47e6c846c997fe2058399b768d5802e264d8a
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 15:35:31 -0300

    Update GenerationsObject.cs

--- Assets/Scripts/Common Objects/GenerationsObject.cs
+++ Assets/Scripts/Common Objects/GenerationsObject.cs
@@ -2,32 +2,6 @@
 
 public class GenerationsObject : RingEngineObject
 {
-    public bool active
-    {
-        get
-        {
-            return Active;
-        }
-        set
-        {
-            if (Active != value)
-            {
-                if (Active == false)
-                {
-                    SendMessage("OnBecomeActive", SendMessageOptions.DontRequireReceiver);
-                }
-                else
-                {
-                    SendMessage("OnBecomeInactive", SendMessageOptions.DontRequireReceiver);
-                }
-            }
-
-            Active = value;
-        }
-    }
-
-    private bool Active;
-
     public int SetObjectID;
 
     public GenerationsObject FindObjectByID(int id)
