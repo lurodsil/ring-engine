5e434570628cc70b74a624a2c5dfcdcba2080633
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:17:45 -0300

    Created Boss Trigger
    Enables boss fight

---
+++ Assets/Scripts/BossTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    public Stage stage;
+
+    public EggmanMobile eggmanMobile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(GameTags.playerTag))
+        {
+            eggmanMobile.active = true;
+            stage.StartBossBattle();
+            gameObject.SetActive(false);
+        }
+
+    }
+}
