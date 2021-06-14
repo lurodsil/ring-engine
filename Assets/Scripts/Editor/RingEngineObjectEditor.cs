d0c753408fe3b4274fc84315fcd41608a3231357
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 17:09:28 -0300

    Update RingEngineObjectEditor.cs

--- Assets/Scripts/Editor/RingEngineObjectEditor.cs
+++ Assets/Scripts/Editor/RingEngineObjectEditor.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(RingEngineObject),true)]
+[CustomEditor(typeof(RingEngineObject), true)]
+[CanEditMultipleObjects]
 public class RingEngineObjectEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         RingEngineObject ringEngineObject = (RingEngineObject)target;
         ringEngineObject.active = EditorGUILayout.Toggle("Active", ringEngineObject.active);
+
         DrawDefaultInspector();
+
+        SerializedProperty onBecomeActive = serializedObject.FindProperty("OnBecomeActive");
+        SerializedProperty onBecomeInactive = serializedObject.FindProperty("OnBecomeInactive");
+
+        ringEngineObject.showEventsInInspector = EditorGUILayout.Foldout(ringEngineObject.showEventsInInspector, "Events");
+
+        if (ringEngineObject.showEventsInInspector)
+        {
+            EditorGUILayout.PropertyField(onBecomeActive);
+            EditorGUILayout.PropertyField(onBecomeInactive);
+        }
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
