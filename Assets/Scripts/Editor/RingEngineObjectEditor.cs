afba7593516f72f287445de5f4b3f9f730a3ea29
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 15:35:24 -0300

    Created Editor Class for Ring Engine Object

---
+++ Assets/Scripts/Editor/RingEngineObjectEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RingEngineObject),true)]
+public class RingEngineObjectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        RingEngineObject ringEngineObject = (RingEngineObject)target;
+        ringEngineObject.active = EditorGUILayout.Toggle("Active", ringEngineObject.active);
+        DrawDefaultInspector();
+    }
+}
