394721962295b2089869512d1c15ba64ccc6d27f
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-06-14 10:16:02 -0300

    Created Billboard Mesh
    Used for Number 10 of super ring

---
+++ Assets/Scripts/BillboardMesh.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BillboardMesh : MonoBehaviour
+{
+    public enum RenderMode
+    {
+        Billboard,
+        VerticalBillboard
+    }
+
+    public RenderMode renderMode;
+
+    Vector3 targetPosition;
+
+    void LateUpdate()
+    {
+        Vector3 cameraPosition = Camera.main.transform.position;
+
+        switch (renderMode)
+        {
+            case RenderMode.Billboard: targetPosition = cameraPosition; break;
+            case RenderMode.VerticalBillboard: targetPosition = new Vector3(cameraPosition.x, transform.position.y, cameraPosition.z); break;
+        }
+
+        transform.LookAt(targetPosition);
+    }
+}
