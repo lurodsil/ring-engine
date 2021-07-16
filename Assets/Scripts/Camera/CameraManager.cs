7b0566ccb00bebee2214d5a1fb88586c0976035d
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2021-07-16 14:37:46 -0300

    Created Camera Manager

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static GameObject activeCamera;
+
+    public static GameObject[] cameras;
+
+    private void Start()
+    {
+        cameras = new GameObject[10];
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < cameras.Length; i++)
+        {
+            if(cameras[i] != null)
+            {
+                activeCamera = cameras[i];
+                break;
+            }
+
+            activeCamera = Camera.main.gameObject;
+        }
+    }
+}
