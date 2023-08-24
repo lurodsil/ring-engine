f5dba393608f0979a35327a5ac932cd58474c6d7
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2023-08-23 22:29:59 -0300

    Update Stage editor folder

---
+++ Assets/Extensions/StageEditor/ObjectPlacer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
