f54e720f4cd5d9e7035d08baf8506a91c5b0c21c
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2024-08-09 10:38:37 -0300

    Renamed FallDeadCollision To FallDeadTrigger

---
+++ Assets/Scripts/Common Objects/Collision/FallDeadTrigger.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class FallDeadTrigger : CommonStatefulObject
+{
+    public bool drownPlayer = false;
+
+    public void Start()
+    {
+        objectState = StateFallDead;
+
+        OnPlayerTriggerEnter.RemoveAllListeners();
+
+        OnPlayerTriggerEnter.AddListener(KillPlayer);
+        OnPlayerTriggerEnter.AddListener(ChangeState);    
+    }
+
+    #region State Fall Dead
+    void StateFallDeadStart()
+    {
+        OnStateStart?.Invoke();
+
+        CameraManager.DeactivateAllCameras();
+
+        MainCamera.SetActive(false);
+    }
+    void StateFallDead()
+    {
+        if (drownPlayer)
+        {
+            player.rigidbody.velocity *= 0.9f;
+        }
+
+        if (Time.time > player.stateMachine.lastStateTime + 2)
+        {
+            GameManager.instance.lives--;
+            GameManager.instance.ReloadSceneWithLoading();
+        }
+    }
+    void StateFallDeadEnd()
+    {
+        OnStateEnd?.Invoke();
+    }
+    #endregion
+
+    private void KillPlayer()
+    {
+        player.drown = drownPlayer;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        GizmosExtension.DrawBoxBoundaries(GetComponent<BoxCollider>());
+    }
+}
