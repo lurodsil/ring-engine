15c61db937eb1d0f798ef79d044141c99aed911c
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2024-08-09 11:04:01 -0300

    Created Player core to handle common functions

---
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class PlayerCore : MonoBehaviour
+{
+    //Player global constants
+    public const float minimunMovementVelocity = 0.1f;
+    public const float deadZone = 0.2f;
+    public const float halfOfOne = 0.5f;
+
+    //Components
+    new public CapsuleCollider collider { get; set; }
+    public new Rigidbody rigidbody { get; set; }
+
+    [Header("Ground Insformation")]
+    public float isGroundedMaxDistance = 0.3f;
+    public float isGroundedRadius = 0.1f;
+    public float groundInformationMaxDistance = 1;
+    public float groundInformationRadius = 0.02f;
+    public LayerMask groundSearchLayerMask;
+
+    private float rigidbodyDragTemp { get; set; }
+
+    public bool IsGrounded(QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.Ignore)
+    {
+        
+        Ray isGroundedRay = new Ray(collider.bounds.center, -transform.up);
+        return Physics.SphereCast(isGroundedRay, isGroundedRadius, isGroundedMaxDistance, groundSearchLayerMask, queryTriggerInteraction);
+    }
+
+    public RaycastHit GetGroundInformation(QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.Ignore)
+    {
+        Ray groundInformationRay = new Ray(collider.bounds.center + transform.forward * rigidbody.ConformVelocity(60) * 0.5f, -transform.up);
+        RaycastHit groundInformationHit = new RaycastHit();
+        Physics.SphereCast(groundInformationRay, groundInformationRadius, out groundInformationHit, groundInformationMaxDistance, groundSearchLayerMask, queryTriggerInteraction);
+        return groundInformationHit;
+    }
+
+
+    public void StoreRigidbodyState()
+    {
+        rigidbodyDragTemp = rigidbody.drag;
+    }
+
+    public void ResetRigidbodyState()
+    {
+        rigidbody.drag = rigidbodyDragTemp;
+        rigidbodyDragTemp = 0;
+    }
+    public virtual void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;    
+        Gizmos.DrawWireSphere(collider.bounds.center - transform.up * ((collider.height * halfOfOne) + isGroundedMaxDistance), isGroundedRadius);
+    }
+
+}
