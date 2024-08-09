4d613c0b77bc5b1f799b8547ea5da8fd4c3181f3
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2024-08-09 10:58:42 -0300

    Update PhysicsMotionValues.cs

--- Assets/Scripts/PhysicsMotionValues.cs
+++ Assets/Scripts/PhysicsMotionValues.cs
@@ -1,9 +1,11 @@
 ﻿[System.Serializable]
 public class PhysicsMotionValues
 {
-    public float accelerationForce = 35;
-    public float decelerationForce = 35;
-    public float brakeMinSpeed = 15;
+    public float accelerationForce = 38;
+    public float accelerationForceInBoost = 100;
+    public float decelerationForce = 20;
+    public float brakeMinSpeed = 1;
     public float brakeForce = 50;       
-    public float maxSpeed = 44;
+    public float maxSpeed = 25;
+    public float maxSpeedInBoost = 35;
 }
