0c2c97f5856db1a7aafc21cd9b44f07e68b66046
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2026-04-18 12:46:42 -0300

    Add wall property to BezierMeshStructure
    Add a new public GameObject 'wall' to BezierMeshStructure and update the constructor to accept and assign the wall parameter. This enables storing an additional wall prefab/component for bezier mesh parts.

--- Assets/Extensions/BezierMeshCreator/Scripts/BezierMeshStructure.cs
+++ Assets/Extensions/BezierMeshCreator/Scripts/BezierMeshStructure.cs
@@ -6,17 +6,19 @@ public class BezierMeshStructure
     public string name;
     public GameObject part;
     public GameObject cap;
+    public GameObject wall;
     public float scale;
     public float colliderScale;
     public ColliderType colliderType;
 
     public BezierMeshStructure() {}
 
-    public BezierMeshStructure(string name, GameObject part, GameObject cap, float scale, ColliderType colliderType)
+    public BezierMeshStructure(string name, GameObject part, GameObject cap, GameObject wall, float scale, ColliderType colliderType)
     {
         this.name = name;
         this.part = part;
         this.cap = cap;
+        this.wall = wall;
         this.scale = scale;
         this.colliderType = colliderType;
     }
