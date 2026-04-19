9c9412887e00901f897cb1c7b23b9705e47e9d8d
Lucas Rodrigues <lucasrodriguesdasilva@outlook.com>
2026-04-19 19:08:10 -0300

    Init moveToPosition and track lastDistance
    Set moveToPosition default to true so camera panning is enabled by default. Update FixedUpdate to assign MainCamera.lastDistance = distance each frame so other systems have the current camera distance. Also ensure file ends with newline.

--- Assets/Scripts/Common Objects/Cameras/ObjCameraPan.cs
+++ Assets/Scripts/Common Objects/Cameras/ObjCameraPan.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ObjCameraPan : CameraCommon
 {
-    public bool moveToPosition;
+    public bool moveToPosition = true;
 
     private void FixedUpdate()
     {
@@ -17,5 +17,6 @@ public class ObjCameraPan : CameraCommon
         Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, fieldOfView, EaseIn());
         cameraTarget.offset = Vector3.Lerp(cameraTarget.offset, targetOffset, EaseIn());
         MainCamera.lastRotation = Camera.main.transform.rotation;
+        MainCamera.lastDistance = distance;
     }
 }
